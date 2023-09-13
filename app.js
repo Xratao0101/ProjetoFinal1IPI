673ceb8d08b5559d19e807def503d0a2e201c6ee
Xratao0101 <cauasantosnicollas@gmail.com>
2023-09-13 17:04:02 -0300

    menu inicial

---
+++ app.js
@@ -0,0 +1,27 @@
+var lista_de_produtos = []
+
+
+
+while (true) {
+
+    var opcao = prompt(`
+    Bem Vindo ao Cadastro de Produtos :3
+
+    1. Cadastra novo produto
+    2.Editar produto
+    3.Remover produtos
+    4.visualizar todos os produtos
+ `)
+    if (opcao == 1) {
+        //codigo para adicionar produto
+    } else if (opcao == 2) {
+        //codigo para editar produto
+    } else if (opcao == 3) {
+        //codigo para remover produto
+    } else if (opcao == 4) {
+        //codigo para visualizar todos os produtos
+    } else {
+        alert("Nenhum das opcoes foi selecionadas!")
+    }
+
+}
